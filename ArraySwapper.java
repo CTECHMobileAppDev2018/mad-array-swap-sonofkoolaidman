f563f5c3f2b9a19fa3c89fccfb00d0109fa257c3
Mr. Bartucz <jobartucz@isd535.org>
2019-02-11 06:08:19 -0600

    Update ArraySwapper.java

--- ArraySwapper.java
+++ ArraySwapper.java
@@ -1,27 +1,63 @@
+import java.util.Arrays;
+
 class ArraySwapper {
   
-  public int[] arraySwap(int []inputArray) {
+  // This method assumes you only have 2 items in your array
+  public void arraySwap(int []inputArray) {
     
     // Swap the two items in "inputArray" and return the output
-    int[] outputArray = new int[2];
-
     // put your code here
-    
-    return outputArray;
-    
+    int temp;
+        
   }
+  
+  // This method swaps any 2 items in any array
+  public void arraySwapAny2(int []inputArray, int loc1, int loc2) {
+    
+    // Swap the two items in "inputArray" and return the output
+    // Remember to make sure that loc1 and loc2 are valid positions in the array!
+    int temp;
+        
+  }
+
+  
   
   public static void main(String[] args) {
 
     ArraySwapper testSwapper = new ArraySwapper();
     
-    int[] exampleInput = {1,2};
-    int[] exampleOutput = testSwapper.arraySwap(exampleInput);
+    // ***** Test the first swap method *******
+    System.out.println("*** Trying first method ***");
+    int[] exampleInput = {1, 2};
+    int[] exampleOutput = {2, 1};
+    System.out.println("exampleInput before: " + Arrays.toString(exampleInput));    
+    System.out.println("exampleOutput: " + Arrays.toString(exampleInput));    
     
-    if (exampleInput[0] != exampleOutput[1] || exampleInput[1] != exampleOutput[0]) {
-      System.out.println("Swap failed");
+    // This calls the first swap method
+    testSwapper.arraySwap(exampleInput);
+    System.out.println("exampleInput after: " + Arrays.toString(exampleInput));    
+    
+    if (java.util.Arrays.equals(exampleInput, exampleOutput)) {
+      System.out.println("arraySwap succeeded");
     } else {
-      System.out.println("Swap succeeded!");
+      System.out.println("arraySwap failed!");
+    }
+    
+    // ***** Test the second swap method *******
+    System.out.println("\n*** Trying second method ***");
+    exampleInput = new int[]{1, 22, 333, 4444};
+    exampleOutput = new int[]{1, 333, 22, 4444};
+    System.out.println("exampleInput before: " + Arrays.toString(exampleInput));    
+    System.out.println("exampleOutput: " + Arrays.toString(exampleOutput));    
+    
+    // This calls the second swap method
+    testSwapper.arraySwapAny2(exampleInput, 1, 2);
+    System.out.println("exampleInput after: " + Arrays.toString(exampleInput));    
+    
+    if (java.util.Arrays.equals(exampleInput, exampleOutput)) {
+      System.out.println("arraySwapAny2 succeeded");
+    } else {
+      System.out.println("arraySwapAny2 failed!");
     }
   }
 }
